fe315ae000715257cd9e3c4a171874d65924a92a
pavankr401 <pavankr401@gmail.com>
2022-12-05 17:45:54 +0530

    feat: some changes in code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const { MongoClient, ObjectId } = require('mongodb');
 
 // create MongoClient instance
-const client = new MongoClient( process.env.MONGO_URI );
+const client = new MongoClient(process.env.MONGO_URI);
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -25,46 +25,56 @@ app.post("/api/users", async function (req, res) {
   res.json({ username: name, _id: getId });
 })
 
-app.get("/api/users", async function (req, res){
+app.get("/api/users", async function (req, res) {
   let allUsers = await getAllUsers();
   res.send(allUsers);
 })
 
-app.post("/api/users/:_id/exercises", async function(req, res){
-  let {description, duration, date} = req.body;
+app.post("/api/users/:_id/exercises", async function (req, res) {
+  let { description, duration, date } = req.body;
   duration = Number(duration);
-  const {_id} = req.params;
+  const { _id } = req.params;
 
-  if(date == "") date = new Date();
-  else if(/^ +$/.test(date)) return res.send("invalid date");
-  else if(!isNaN(Number(date))) date = new Date(Number(date));
+  if (date == "") date = new Date();
+  else if (/^ +$/.test(date)) return res.send("invalid date");
   else date = new Date(date);
 
   let username = await checkUserById(_id);
-  if(username && !isNaN(duration) && !isNaN(date.getTime()) ){
-    await findUserByIdAndUpdate(_id, duration, description, date.toDateString() );
-    res.json({"_id": _id, "username": username, "date": date.toDateString(), "duration": duration, "description": description})
+  if (username && !isNaN(duration) && !isNaN(date.getTime())) {
+    await findUserByIdAndUpdate(_id, duration, description, date.toDateString());
+    res.send({ "username": username, "description": description, "duration": duration, "date": date.toDateString(), "_id": _id })
   }
-  else{
+  else {
     // console.log(duration, date, username);
     res.send("invalid id")
   }
 })
 
 app.get("/api/users/:_id/logs", async function(req, res){
+  const {_id} = req.params;
   let {from, to, limit} = req.query;
-  if(!isNaN(Number(from))) from = new Date(Number(from));
-  else from = new Date(from);
-  if(!isNaN(Number(to))) to = new Date(Number(to));
-  else to = new Date(to);
-  limit = Number(limit);
-  let {_id} = req.params;
-  let {username, log} = await getUserLogs(_id,from,to,limit);
-  if(username && from == "Invalid Date") res.json({"_id": _id, "username": username, "count": log.length, "log": log});
-  else if(username && from != "Invalid Date" && to != "Invalid Date") res.json({"_id": _id, "username": username, "from": from.toDateString(), "to": to.toDateString(), "count": log.length, "log": log});
-  else res.send("user not found");
-})
 
+  if(_id.length != 24) res.send("invalid id");
+
+  from = new Date(from);
+  to = new Date(to);
+  limit = Number(limit);
+
+  let result = await getLogs(_id, from, to, limit);
+
+  
+  if(result && (from == "Invalid Date" || to == "Invalid Date")){
+    let {username, log} = result;
+    res.send({username: username, count: log.length, _id: _id, log: log});
+  }
+  else if(result && from != "Invalid Date" && to != "Invalid Date"){
+    let {username, log} = result;
+    res.send({username: username, count: log.length, _id: _id, from: from.toDateString(), to: to.toDateString(), log: log});
+  }
+  else{
+    res.send("invalid user")
+  }
+})
 
 
 
@@ -74,7 +84,7 @@ async function insertUser(name) {
   let finalRes;
   try {
     await client.connect();
-    let documentToInsert = { username: name, count: 0, log: []};
+    let documentToInsert = { username: name, count: 0, log: [] };
     let result = await usersCollection.insertOne(documentToInsert);
     finalRes = result.insertedId;
   }
@@ -87,104 +97,104 @@ async function insertUser(name) {
   }
 }
 
-async function getAllUsers(){
+async function getAllUsers() {
   let finalRes;
-  try{
+  try {
     await client.connect();
-    finalRes = await usersCollection.find({}, {projection: {username: 1}}).toArray();
+    finalRes = await usersCollection.find({}, { projection: { username: 1 } }).toArray();
   }
-  catch(err){
+  catch (err) {
     console.error(err);
   }
-  finally{
+  finally {
     await client.close();
     return finalRes;
   }
 }
 
-async function checkUserById(id){
+async function checkUserById(id) {
   let finalRes;
-  try{
+  try {
     await client.connect();
-    let filterDoc = { _id: new ObjectId(id)};
+    let filterDoc = { _id: new ObjectId(id) };
     let result = await usersCollection.findOne(filterDoc);
-    if(result) finalRes = result.username;
+    if (result) finalRes = result.username;
     else finalRes = false;
     // console.log(result);
-  } 
-  catch(error){
+  }
+  catch (error) {
     console.log(error);
   }
-  finally{
+  finally {
     client.close();
     return finalRes;
   }
 }
 
-async function findUserByIdAndUpdate(id, dur, des, date){
+async function findUserByIdAndUpdate(id, dur, des, date) {
   let finalRes;
-  try{
+  try {
     await client.connect();
-    let filterDoc = {_id: new ObjectId(id)};
+    let filterDoc = { _id: new ObjectId(id) };
     let updateDoc = {
-      $inc: {count : 1},
-      $push: {log: {
-        description: des,
-        duration: dur,
-        date: date
-      }}
+      $inc: { count: 1 },
+      $push: {
+        log: {
+          description: des,
+          duration: dur,
+          date: date
+        }
+      }
     };
-    let optionsObj = { returnDocument: 'after'}
+    let optionsObj = { returnDocument: 'after' }
     let result = await usersCollection.findOneAndUpdate(filterDoc, updateDoc, optionsObj);
-    if(result){
+    if (result) {
       finalRes = true;
     }
-    else{
+    else {
       finalRes = false;
     }
   }
-  catch(error){
+  catch (error) {
     console.log(error);
   }
-  finally{
+  finally {
     await client.close();
     return finalRes;
   }
 }
 
-async function getUserLogs(id, from, to, lim){
+async function getLogs(id, from, to, limit){
   let finalRes;
   try{
     await client.connect();
-    let filterDoc,result,options; 
+    let filterDoc = {_id: new ObjectId(id)};
+    let options = {projection: {username: 1, log: 1, _id: 0}};
+    let result = await usersCollection.findOne(filterDoc, options);
 
-    filterDoc = {_id: new ObjectId(id)};
-      options = {projection: {log:1, username:1, _id: 0}};
-      result = await usersCollection.findOne(filterDoc,options);
+    result.log.sort((a,b) => {
+      let x = new Date(a.date);
+      let y = new Date(b.date);
+      if(x >= y) return -1;
+      else return 1;
+    });//console.log(result);
 
     if(from != "Invalid Date" && to != "Invalid Date"){
       result.log = result.log.filter((eachLog) => {
         let date = new Date(eachLog.date);
-        if(date >= from && date < to) return eachLog;
+        if(date >= from && date <= to) return eachLog;
       })
     }
-    result.log.sort((x,y) => {
-      let a = new Date (x.date);
-      let b = new Date (y.date);
-      if(a < b) return 1;
-      if(a >= b) return -1;
-    });
 
-    if(!isNaN(lim)){
-      result.log = result.log.slice(0,lim);
+    if(!isNaN(limit)){
+      result.log = result.log.slice(0, limit);
     }
 
     if(result) finalRes = result;
     else finalRes = false;
+
   }
-  catch(err){
-    console.error(err);
-  }
+  catch(err) {console.error(err)}
   finally{
     await client.close();
     return finalRes;
